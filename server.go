034cafae61f6ada9d7abc461a4db4d928a848087
Gokul Js <gokuljs029@gmail.com>
2026-04-27 02:30:15 +0530

    Refactor gRPC server implementation by removing the Greeter service and enabling multiple server instances. The server now starts on dynamic ports and logs server status. Update ping.proto to remove the Greeter service definition.
    simple three server implementation

--- server.go
+++ server.go
@@ -1,43 +1,36 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
-
-	pb "github.com/gokuljs/graft/proto"
+	"sync"
 
 	"google.golang.org/grpc"
 )
 
-// server implements the Greeter service
-type server struct {
-	pb.UnimplementedGreeterServer
-}
-
-// SayHello implements the SayHello RPC
-func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
-	message := fmt.Sprintf("Hello %s!", req.Name)
-	return &pb.HelloResponse{Message: message}, nil
+func startServer(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	port := 6000 + id
+	addr := fmt.Sprintf(":%d", port)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to start the server on port %d: %v", port, err)
+	}
+	log.Printf("Server %d started on port %d", id, port)
+	s := grpc.NewServer()
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Server %d failed to serve: %v", id, err)
+	}
 }
 
 func main() {
-	// Listen on TCP port 50051
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	defaultNumServers := 5
+	var wg sync.WaitGroup
+	for i := 0; i < defaultNumServers; i++ {
+		wg.Add(1)
+		go startServer(i, &wg)
 	}
-
-	// Create a gRPC server
-	s := grpc.NewServer()
-
-	// Register the Greeter service
-	pb.RegisterGreeterServer(s, &server{})
-	log.Println("Server is running on port 50051...")
-
-	// Start serving
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	wg.Wait()
+	log.Println("All servers started")
 }
